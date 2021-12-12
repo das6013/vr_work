ea148055d02e038cfa66608e1260932cfb490449
Das3013 <46927879+das6013@users.noreply.github.com>
2021-12-12 17:40:34 +0700

    Update LightTest1.cs

--- LightTest1.cs
+++ LightTest1.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class LightTest1 : MonoBehaviour
 {
-    public Transform obj1;
     float distance;
     public Light light;
-   
+
 
     float LightIntensity(Light light, float distance)
     {
+       
         return light.intensity * (1f - Mathf.Clamp01(distance / light.range));
     }
     // Update is called once per frame
-
+   
     void Update()
     {
-        
-       distance = Vector3.Distance(light.transform.position, obj1.position);
+ 
+       distance = Vector3.Distance(light.transform.position, transform.position);
+
+       RaycastHit hit;
+       Ray ray = new Ray(transform.position, light.transform.position - transform.position);
+        Physics.Raycast(ray, out hit);
+        Debug.Log(hit.collider);
+        if (hit.collider.name == "sun")
             Debug.Log(LightIntensity(light, distance));
-       
+        else Debug.Log(0);
+
+
+
+
+
+
     }
 }
